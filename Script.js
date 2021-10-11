4e2eb8b52c1f1aea2905102eb2db9f4687712421
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2021-10-11 21:36:30 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,35 @@
+let countdown = 11;
+document.querySelector(".Countdown").innerText = --countdown;
+setTimeout (() =>{
+    document.querySelector(".Countdown").innerText = --countdown;
+    setTimeout (() =>{
+        document.querySelector(".Countdown").innerText = --countdown;
+        setTimeout (() =>{
+            document.querySelector(".Countdown").innerText = --countdown;
+            setTimeout (() =>{
+                document.querySelector(".Countdown").innerText = --countdown;
+                setTimeout (() =>{
+                    document.querySelector(".Countdown").innerText = --countdown;
+                    setTimeout (() =>{
+                        document.querySelector(".Countdown").innerText = --countdown;
+                        setTimeout (() =>{
+                            document.querySelector(".Countdown").innerText = --countdown;
+                            setTimeout (() =>{
+                                document.querySelector(".Countdown").innerText = --countdown;
+                                setTimeout (() =>{
+                                    document.querySelector(".Countdown").innerText = --countdown;
+                                    setTimeout (() =>{
+                                        document.querySelector(".Countdown").innerText = "Happy Independence Day";
+                                        setTimeout (() =>{
+                                        
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
